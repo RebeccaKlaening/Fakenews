082ef7cad3566c94100720d9b5ee8c002e227a42
RebeccaKlaening <klaeningrebecca@gmail.com>
2018-10-17 11:47:43 +0200

    added files

---
+++ data.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+// This is the file where you can keep your data arrays such as articles and
+// authors.
+
+
+$authors = [
+
+    ['name' => 'J.K Rowland', 'title' => 'Harry Potter Philosopher\'s Stone', 'year' => 1997],
+    ['name' => 'Stephen King', 'title' => 'IT', 'year' => 1986],
+    ['name' => 'John Ajvide Lindqvist.', 'title' => 'Låt den rätte komma in', 'year' => 2004]
+
+];
