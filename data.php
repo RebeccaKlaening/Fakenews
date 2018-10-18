a8d51037257f186ee2bdb8ed8b16f9224809364b
RebeccaKlaening <klaeningrebecca@gmail.com>
2018-10-18 11:47:11 +0200

    made a html template which loops through data

--- data.php
+++ data.php
@@ -3,11 +3,69 @@ declare(strict_types=1);
 // This is the file where you can keep your data arrays such as articles and
 // authors.
 
-
 $authors = [
 
-    ['name' => 'J.K Rowland', 'title' => 'Harry Potter Philosopher\'s Stone', 'year' => 1997],
-    ['name' => 'Stephen King', 'title' => 'IT', 'year' => 1986],
-    ['name' => 'John Ajvide Lindqvist.', 'title' => 'Låt den rätte komma in', 'year' => 2004]
+		['id' => 1,
+		'name' => ' J.K Rowland'
+		],
 
-];
+		['id' => 2,
+		'name' => ' Stephen King'
+		],
+
+		['id' => 3,
+		'name' => ' John Ajvide Lindqvist'
+		],
+
+		['id' => 4,
+		'name' => ' J.R.R Tolkiens'
+		],
+
+		['id' => 5,
+		'name' => ' Sophie Kinsella'
+		],
+
+	];
+
+
+$data = [
+  [
+    'author' => 1,
+    'title' => ' Harry Potter Philosopher\'s Stone',
+    'published date' => '1997-05-25',
+    'content' => 'It was a dog. It was a big dog.',
+    'like Counter' => 44,
+  ],
+
+  [
+    'author' => 2,
+    'title' => ' IT',
+    'published date' => '1986-10-31',
+    'content' => 'It\'s not just your name, sir. It\'s your father\'s name. And it\'s all that\'s left of him. Don\'t destroy it.',
+    'like Counter' => 52
+  ],
+
+  [
+    'author' => 3,
+    'title' => ' Låt den rätte komma in',
+    'published date' => '2004-09-10',
+    'content' => 'To manipulate the fears in others, you must first master your own. Are you ready to begin?Let me get this straight. You think that your client, one of the wealthiest, most powerful men in the world is secretly a vigilante who spends his nights beating criminals to a pulp with his bare hands and your plan is to blackmail this person? Good luck.',
+    'Like Counter' => 23
+  ],
+  [
+    'author' => 4,
+    'title' => ' Lord of the Rings',
+    'published date' => '1954-06-02',
+    'content' => 'And then here there can be no true despair without hope. So as I terrorize Gotham, I will feed its people hope to poison their souls.Are you so desperate to fight criminals that you lock yourself in to take them on one at a time? Okay. Now... Hardened Kevlar plates over titanium-dipped, tri-weave fibers for flexibility. You\'ll be lighter, faster, more agile.',
+    'like Counter' => 67
+  ],
+
+  [
+    'author' => 5,
+    'title' => ' Confessions of a Shopaholic',
+    'published date' => '2001-02-14',
+    'content' => 'You can watch me torture an entire city. And then when you\'ve truly understood the depth of your failure, we will fulfill Ra\'s Al Ghul\'s destiny. We will destroy Gotham.And then, when that is done, and Gotham is... ashes Then you have my permission to die.',
+    'like Counter' => 38
+  ],
+
+  ];
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 declare(strict_types=1);
+require __DIR__.'/data.php';
+require __DIR__.'/functions.php';
+
 // This is the file where you can keep your HTML markup. We should always try to
 // keep us much logic out of the HTML as possible. Put the PHP logic in the top
 // of the files containing HTML or even better; in another PHP file altogether.
@@ -11,8 +14,28 @@ declare(strict_types=1);
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title></title>
+        <title>Fake News</title>
+
+		  <style>
+
+		  .container {
+
+		  }
+
+		  </style>
     </head>
+
+
+
     <body>
+		 <?php foreach($data as $item): ?>
+		 		<div class="container">
+					<h2><?php echo $item['title']; ?></h2>
+						<p><?php echo $item['content']; ?></p>
+						<p><?php echo $item['published date']; ?></p>
+
+			<?php endforeach; ?>
+			</div>
+
     </body>
 </html>
