a7ba74a200c8830e1a75adba1212a45e24c98338
RebeccaKlaening <klaeningrebecca@gmail.com>
2018-10-26 19:12:33 +0200

    made another function to get the authors name from the id

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 // This is the file where you can keep all your functions. Remember to NOT
 
+// a function to sort the articles after published date.
 function sortByDate ($a, $b) {
 	 return strtotime($a['publishedDate']) < strtotime($b['publishedDate']);
 }
@@ -9,14 +10,13 @@ usort($data, 'sortByDate');
 
 
 
-
+// a function to search for the authors name .
 function getName(int $searchId, array $authors): string {
 		foreach ($authors as $author) {
 				$id = $author ['id'];
 					if ($id == $searchId) {
 						return $author ['name'];
 					}
-
 		}
 
 };
