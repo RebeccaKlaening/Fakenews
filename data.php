0dbf33ce08b36f4d25a1ad210fc9926678f2b0f6
RebeccaKlaening <klaeningrebecca@gmail.com>
2018-10-30 12:34:46 +0100

    deleted my image data

--- data.php
+++ data.php
@@ -5,128 +5,127 @@ declare(strict_types=1);
 
 $authors = [
 
-		['id' => 1,
-		'name' => ' J.K Rowland'
-		],
+	['id' => 1,
+	'name' => ' J.K Rowland'
+],
 
-		['id' => 2,
-		'name' => ' Stephen King'
-		],
+['id' => 2,
+'name' => ' Stephen King'
+],
 
-		['id' => 3,
-		'name' => ' John Ajvide Lindqvist'
-		],
+['id' => 3,
+'name' => ' John Ajvide Lindqvist'
+],
 
-		['id' => 4,
-		'name' => ' J.R.R Tolkiens'
-		],
+['id' => 4,
+'name' => ' J.R.R Tolkiens'
+],
 
-		['id' => 5,
-		'name' => ' Sophie Kinsella'
-		],
+['id' => 5,
+'name' => ' Sophie Kinsella'
+],
 
-	];
+];
 
 
 $data = [
-  [
-    'author' => 1,
-    'title' => ' Harry Potter Philosopher\'s Stone',
-    'publishedDate' => '2018-08-09',
-    'content' => file_get_contents(__DIR__.'/text/text01.txt'),
-    'likeCounter' => 44
-	 //'image' =>'image.jpg/jkrowling.jpeg'
+	[
+		'author' => 1,
+		'title' => ' Harry Potter Philosopher\'s Stone',
+		'publishedDate' => '2018-08-09',
+		'content' => file_get_contents(__DIR__.'/text/text01.txt'),
+		'likeCounter' => 44
 
 
 
-  ],
 
-  [
-    'author' => 2,
-    'title' => ' IT',
-    'publishedDate' => '2018-04-11',
-    'content' => file_get_contents(__DIR__.'/text/text02.txt'),
-    'likeCounter' => 52
-	 //'image' => 'image.jpg/king.jpeg'
+	],
 
-  ],
+	[
+		'author' => 2,
+		'title' => ' IT',
+		'publishedDate' => '2018-04-11',
+		'content' => file_get_contents(__DIR__.'/text/text02.txt'),
+		'likeCounter' => 52
 
-  [
-    'author' => 3,
-    'title' => ' Låt den rätte komma in',
-    'publishedDate' => '2018-05-23',
-    'content' => file_get_contents(__DIR__.'/text/text03.txt'),
-    'likeCounter' => 23
-	 //'image' => 'image.jpg/john.jpeg'
 
-  ],
+	],
 
-  [
-    'author' => 4,
-    'title' => ' Lord of the Rings',
-    'publishedDate' => '2018-02-15',
-    'content' => file_get_contents(__DIR__.'/text/text04.txt'),
-	 'likeCounter' => 87
-	 //'image' => 'image.jpg/jrrt.jpg'
+	[
+		'author' => 3,
+		'title' => ' Låt den rätte komma in',
+		'publishedDate' => '2018-05-23',
+		'content' => file_get_contents(__DIR__.'/text/text03.txt'),
+		'likeCounter' => 23
 
-  ],
 
-  [
-    'author' => 5,
-    'title' => ' Confessions of a Shopaholic',
-    'publishedDate' => '2017-12-17',
-    'content' => file_get_contents(__DIR__.'/text/text05.txt'),
-    'likeCounter' => 38
-	 //'image' => 'image.jpg/190px-Sophie_Kinsella.jpg'
+	],
 
-  ],
+	[
+		'author' => 4,
+		'title' => ' Lord of the Rings',
+		'publishedDate' => '2018-02-15',
+		'content' => file_get_contents(__DIR__.'/text/text04.txt'),
+		'likeCounter' => 87
 
-  [
-    'author' => 1,
-    'title' => 'Harry Potter and the Chamber of Secrets',
-    'publishedDate' => '2017-10-27',
-    'content' => file_get_contents(__DIR__.'/text/text06.txt'),
-    'likeCounter' => 44
-	 //'image' => 'image.jpg/jkrowling.jpeg'
 
-  ],
+	],
 
-  [
-    'author' => 2,
-    'title' => 'The Shining',
-    'publishedDate' => '2017-11-04',
-    'content' => file_get_contents(__DIR__.'/text/text07.txt'),
-    'likeCounter' => 52
-	 //'image' => 'image.jpg/king.jpeg'
+	[
+		'author' => 5,
+		'title' => ' Confessions of a Shopaholic',
+		'publishedDate' => '2017-12-17',
+		'content' => file_get_contents(__DIR__.'/text/text05.txt'),
+		'likeCounter' => 38
 
-  ],
 
-  [
-    'author' => 3,
-    'title' => 'Hanteringen av odöda',
-    'publishedDate' => '2017-08-18',
-    'content' => file_get_contents(__DIR__.'/text/text08.txt'),
-    'likeCounter' => 23
-	 //'image' => 'image.jpg/john.jpeg'
+	],
 
-  ],
+	[
+		'author' => 1,
+		'title' => 'Harry Potter and the Chamber of Secrets',
+		'publishedDate' => '2017-10-27',
+		'content' => file_get_contents(__DIR__.'/text/text06.txt'),
+		'likeCounter' => 44
 
-  [
-    'author' => 4,
-    'title' => 'The Two Towers',
-    'publishedDate' => '2017-06-18',
-    'content' => file_get_contents(__DIR__.'/text/text09.txt'),
-    'likeCounter' => 67
-	 //'image' => 'image.jpg/jrrt.jpg'
 
-  ],
+	],
 
-  [
-    'author' => 5,
-    'title' => 'Shopaholic Abroad ',
-    'publishedDate' => '2017-05-19',
-    'content' => file_get_contents(__DIR__.'/text/text10.txt'),
-    'likeCounter' => 38
-	 //'image' => 'image.jpg/190px-Sophie_Kinsella.jpg'
+	[
+		'author' => 2,
+		'title' => 'The Shining',
+		'publishedDate' => '2017-11-04',
+		'content' => file_get_contents(__DIR__.'/text/text07.txt'),
+		'likeCounter' => 52
+
+
+	],
+
+	[
+		'author' => 3,
+		'title' => 'Hanteringen av odöda',
+		'publishedDate' => '2017-08-18',
+		'content' => file_get_contents(__DIR__.'/text/text08.txt'),
+		'likeCounter' => 23
+
+
+	],
+
+	[
+		'author' => 4,
+		'title' => 'The Two Towers',
+		'publishedDate' => '2017-06-18',
+		'content' => file_get_contents(__DIR__.'/text/text09.txt'),
+		'likeCounter' => 67
+
+	],
+
+	[
+		'author' => 5,
+		'title' => 'Shopaholic Abroad ',
+		'publishedDate' => '2017-05-19',
+		'content' => file_get_contents(__DIR__.'/text/text10.txt'),
+		'likeCounter' => 38
+
 	]
-  ];
+];
